a40cc07b4cb616fd928636c4da40a9f2f0177a4c
MahajanPyanshu <tarzan144144@gmail.com>
2026-06-06 19:04:18 +0200

    Sixth Program

--- src/Main5.java
+++ src/Main5.java
@@ -1,44 +1,62 @@
-interface Payable{
-    double calculatePay();
+import java.util.Scanner;
+class Vehicle{
+    public void start(){
+        System.out.println("Starting the vehicle");
+    }
+    public void stop(){
+        System.out.println("Stopping the vehicle");
+    }
+    public void fuelType(String fuel){
+        System.out.println("This vehicle takes the fuel type "+fuel);
+    }
 }
-interface Taxable{
-    double calculateTax();
+class ElectricCar extends Vehicle{
+    @Override
+    public void start(){
+        System.out.println("Starting my electric car");
+    }
+    public void stop(){
+        System.out.println("Stopping my Electric car");
+    }
+    public void fuelType(String fuel){
+        System.out.println("This vehicle uses "+fuel);
+    }
 }
-class Employee implements Payable,Taxable{
-    private double baseSalary;
-    private double hoursWorked;
-    public double calculatePay(){
-        return baseSalary*hoursWorked;
+class PetrolCar extends Vehicle{
+    @Override
+    public void start(){
+        System.out.println("Starting my Petrol car");
     }
-
-    public void setBaseSalary(double baseSalary) {
-        this.baseSalary = baseSalary;
+    public void stop(){
+        System.out.println("Stopping my Petrol car");
     }
-
-    public void setHoursWorked(double hoursWorked) {
-        this.hoursWorked = hoursWorked;
+    public void fuelType(String fuel){
+        System.out.println("This vehicle uses "+fuel);
     }
-
-    public double getHoursWorked() {
-        return hoursWorked;
+}
+class Bicycle extends Vehicle{
+    @Override
+    public void start(){
+        System.out.println("Starting my Bicycle");
     }
-
-    public double getBaseSalary() {
-        return baseSalary;
+    public void stop(){
+        System.out.println("Stopping my Bicycle");
     }
-
-    public double calculateTax(){
-        return 0.1*calculatePay();
-    }
-    public double netSalary(){
-        return calculatePay()-calculateTax();
+    public void fuelType(String fuel){
+        System.out.println("This vehicle uses "+fuel);
     }
 }
 public class Main5{
+    static Scanner s=new Scanner(System.in);
     public static void main(String[] args) {
-        Employee obj=new Employee();
-        obj.setBaseSalary(20);
-        obj.setHoursWorked(100);
-        System.out.println(obj.netSalary());
+
+        Vehicle[]arr={new ElectricCar(),new PetrolCar(),new Bicycle()};
+        for(Vehicle a:arr){
+            a.start();
+            a.stop();
+            System.out.println("Enter the type of fuel");
+            String f=s.next();
+            a.fuelType(f);
+        }
     }
 }
